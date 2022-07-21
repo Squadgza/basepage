0154e52745392de833872604d8992c76a5a00e9c
Gints Misins <88575586+GintsM@users.noreply.github.com>
2022-07-21 16:19:39 +0300

    logo component

---
+++ src/components/navbar/logo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from '../../assets/logo/logo_200.png';
+
+const Logo = () => {
+  return (
+    <div className='logoContainer'>
+      <img src={logo} alt='' />
+    </div>
+  )
+}
+
+export default Logo
