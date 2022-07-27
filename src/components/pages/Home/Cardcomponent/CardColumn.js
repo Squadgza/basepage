025761fe46996ac13ade679b6202e563e3919f5e
aakbarkhan <aakbarkhan@gmail.com>
2022-07-27 14:48:18 +0530

    add the npm react infinite scroll component

--- src/components/pages/Home/Cardcomponent/CardColumn.js
+++ src/components/pages/Home/Cardcomponent/CardColumn.js
@@ -1,16 +1,46 @@
-import React from 'react'
-import Card from './Card'
+import React, { useState } from 'react';
+// import Card from './Card'
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 
 const CardColumn = ({ data }) => {
     console.log(data, 'from dummy data');
-    return (
-    <div>
-        { data.map((item) => (
-            <Card key={item.key} data={item} />
+    const [count, setCount] = useState({
+        prev: 0,
+        next: 3
+      })
+      const [hasMore, setHasMore] = useState(true);
+      const [current, setCurrent] = useState(data.slice(count.prev, count.next))
 
-        ))}
-    </div>
+      const getMoreData = () => {
+        if (current.length === data.length) {
+          setHasMore(false);
+          return;
+        }
+        setTimeout(() => {
+          setCurrent(current.concat(data.slice(count.prev + 3, count.next + 3)))
+        }, 2000)
+        setCount((prevState) => ({ prev: prevState.prev + 3, next: prevState.next + 3 }))
+      }
+
+    return (
+        <InfiniteScroll
+        dataLength={current.length}
+        next={getMoreData}
+        hasMore={hasMore}
+        loader={<h4>Loading...</h4>}
+      >
+        <div>
+          {current && current.map(((item, index) => (
+            <div key={item.key} className="post">
+              <h4>{item.topic}</h4>
+              <img src={item.image} alt='img'/>
+              <button>Details</button>
+            </div>
+          )))
+          }
+        </div>
+      </InfiniteScroll>
   )
 }
 
--- src/dummydata.js
+++ src/dummydata.js
@@ -13,6 +13,111 @@ description: 'Description',
 key:1,
 post: 'Wasura urubuga irembo.gov.rw Kanda Hano Kureba Time Table y’ibizamini  Sura urubuga rwa Traffic Police (RNP: Rwanda National Police), Kanda hano',
 location:'embadded google image'
+},
+{topic: 'KWIYANDIKISHA Gukora IBIZAMINI bya Permis Provisoir na Definitif (Byo gutwara ibinyabiziga) bya Traffic Police (RNP) Byasubukuwe GUHERA 15/07/2022 KUGEZA 22/07/2022, Rwanda.',
+image: 'https://kigalifinder.com/wp-content/uploads/classified-listing/2022/07/police.jpg',
+description: 'Description',
+// TODO: KEY SHOULDE HAVE A UNIQUE VALUE.
+key:2,
+post: 'Wasura urubuga irembo.gov.rw Kanda Hano Kureba Time Table y’ibizamini  Sura urubuga rwa Traffic Police (RNP: Rwanda National Police), Kanda hano',
+location:'embadded google image'
+},
+{topic: '48 job available in University of Rwanda (UR) Deadline 28July 2022',
+image: 'https://kigalifinder.com/wp-content/uploads/classified-listing/2022/07/ur.jpg',
+description: 'Description',
+key:3,
+post: 'Wasura urubuga irembo.gov.rw Kanda Hano Kureba Time Table y’ibizamini  Sura urubuga rwa Traffic Police (RNP: Rwanda National Police), Kanda hano',
+location:'embadded google image'
+},
+{topic: 'KWIYANDIKISHA Gukora IBIZAMINI bya Permis Provisoir na Definitif (Byo gutwara ibinyabiziga) bya Traffic Police (RNP) Byasubukuwe GUHERA 15/07/2022 KUGEZA 22/07/2022, Rwanda.',
+image: 'https://kigalifinder.com/wp-content/uploads/classified-listing/2022/07/police.jpg',
+description: 'Description',
+// TODO: KEY SHOULDE HAVE A UNIQUE VALUE.
+key:4,
+post: 'Wasura urubuga irembo.gov.rw Kanda Hano Kureba Time Table y’ibizamini  Sura urubuga rwa Traffic Police (RNP: Rwanda National Police), Kanda hano',
+location:'embadded google image'
+},
+{topic: '48 job available in University of Rwanda (UR) Deadline 28July 2022',
+image: 'https://kigalifinder.com/wp-content/uploads/classified-listing/2022/07/ur.jpg',
+description: 'Description',
+key:5,
+post: 'Wasura urubuga irembo.gov.rw Kanda Hano Kureba Time Table y’ibizamini  Sura urubuga rwa Traffic Police (RNP: Rwanda National Police), Kanda hano',
+location:'embadded google image'
+},
+{topic: 'KWIYANDIKISHA Gukora IBIZAMINI bya Permis Provisoir na Definitif (Byo gutwara ibinyabiziga) bya Traffic Police (RNP) Byasubukuwe GUHERA 15/07/2022 KUGEZA 22/07/2022, Rwanda.',
+image: 'https://kigalifinder.com/wp-content/uploads/classified-listing/2022/07/police.jpg',
+description: 'Description',
+// TODO: KEY SHOULDE HAVE A UNIQUE VALUE.
+key:6,
+post: 'Wasura urubuga irembo.gov.rw Kanda Hano Kureba Time Table y’ibizamini  Sura urubuga rwa Traffic Police (RNP: Rwanda National Police), Kanda hano',
+location:'embadded google image'
+},
+{topic: '48 job available in University of Rwanda (UR) Deadline 28July 2022',
+image: 'https://kigalifinder.com/wp-content/uploads/classified-listing/2022/07/ur.jpg',
+description: 'Description',
+key:7,
+post: 'Wasura urubuga irembo.gov.rw Kanda Hano Kureba Time Table y’ibizamini  Sura urubuga rwa Traffic Police (RNP: Rwanda National Police), Kanda hano',
+location:'embadded google image'
+},
+{topic: 'KWIYANDIKISHA Gukora IBIZAMINI bya Permis Provisoir na Definitif (Byo gutwara ibinyabiziga) bya Traffic Police (RNP) Byasubukuwe GUHERA 15/07/2022 KUGEZA 22/07/2022, Rwanda.',
+image: 'https://kigalifinder.com/wp-content/uploads/classified-listing/2022/07/police.jpg',
+description: 'Description',
+// TODO: KEY SHOULDE HAVE A UNIQUE VALUE.
+key:8,
+post: 'Wasura urubuga irembo.gov.rw Kanda Hano Kureba Time Table y’ibizamini  Sura urubuga rwa Traffic Police (RNP: Rwanda National Police), Kanda hano',
+location:'embadded google image'
+},
+{topic: '48 job available in University of Rwanda (UR) Deadline 28July 2022',
+image: 'https://kigalifinder.com/wp-content/uploads/classified-listing/2022/07/ur.jpg',
+description: 'Description',
+key:9,
+post: 'Wasura urubuga irembo.gov.rw Kanda Hano Kureba Time Table y’ibizamini  Sura urubuga rwa Traffic Police (RNP: Rwanda National Police), Kanda hano',
+location:'embadded google image'
+},
+{topic: 'KWIYANDIKISHA Gukora IBIZAMINI bya Permis Provisoir na Definitif (Byo gutwara ibinyabiziga) bya Traffic Police (RNP) Byasubukuwe GUHERA 15/07/2022 KUGEZA 22/07/2022, Rwanda.',
+image: 'https://kigalifinder.com/wp-content/uploads/classified-listing/2022/07/police.jpg',
+description: 'Description',
+// TODO: KEY SHOULDE HAVE A UNIQUE VALUE.
+key:10,
+post: 'Wasura urubuga irembo.gov.rw Kanda Hano Kureba Time Table y’ibizamini  Sura urubuga rwa Traffic Police (RNP: Rwanda National Police), Kanda hano',
+location:'embadded google image'
+},
+{topic: '48 job available in University of Rwanda (UR) Deadline 28July 2022',
+image: 'https://kigalifinder.com/wp-content/uploads/classified-listing/2022/07/ur.jpg',
+description: 'Description',
+key:11,
+post: 'Wasura urubuga irembo.gov.rw Kanda Hano Kureba Time Table y’ibizamini  Sura urubuga rwa Traffic Police (RNP: Rwanda National Police), Kanda hano',
+location:'embadded google image'
+},
+{topic: 'KWIYANDIKISHA Gukora IBIZAMINI bya Permis Provisoir na Definitif (Byo gutwara ibinyabiziga) bya Traffic Police (RNP) Byasubukuwe GUHERA 15/07/2022 KUGEZA 22/07/2022, Rwanda.',
+image: 'https://kigalifinder.com/wp-content/uploads/classified-listing/2022/07/police.jpg',
+description: 'Description',
+// TODO: KEY SHOULDE HAVE A UNIQUE VALUE.
+key:12,
+post: 'Wasura urubuga irembo.gov.rw Kanda Hano Kureba Time Table y’ibizamini  Sura urubuga rwa Traffic Police (RNP: Rwanda National Police), Kanda hano',
+location:'embadded google image'
+},
+{topic: '48 job available in University of Rwanda (UR) Deadline 28July 2022',
+image: 'https://kigalifinder.com/wp-content/uploads/classified-listing/2022/07/ur.jpg',
+description: 'Description',
+key:13,
+post: 'Wasura urubuga irembo.gov.rw Kanda Hano Kureba Time Table y’ibizamini  Sura urubuga rwa Traffic Police (RNP: Rwanda National Police), Kanda hano',
+location:'embadded google image'
+},
+{topic: 'KWIYANDIKISHA Gukora IBIZAMINI bya Permis Provisoir na Definitif (Byo gutwara ibinyabiziga) bya Traffic Police (RNP) Byasubukuwe GUHERA 15/07/2022 KUGEZA 22/07/2022, Rwanda.',
+image: 'https://kigalifinder.com/wp-content/uploads/classified-listing/2022/07/police.jpg',
+description: 'Description',
+// TODO: KEY SHOULDE HAVE A UNIQUE VALUE.
+key:14,
+post: 'Wasura urubuga irembo.gov.rw Kanda Hano Kureba Time Table y’ibizamini  Sura urubuga rwa Traffic Police (RNP: Rwanda National Police), Kanda hano',
+location:'embadded google image'
+},
+{topic: '48 job available in University of Rwanda (UR) Deadline 28July 2022',
+image: 'https://kigalifinder.com/wp-content/uploads/classified-listing/2022/07/ur.jpg',
+description: 'Description',
+key:15,
+post: 'Wasura urubuga irembo.gov.rw Kanda Hano Kureba Time Table y’ibizamini  Sura urubuga rwa Traffic Police (RNP: Rwanda National Police), Kanda hano',
+location:'embadded google image'
 }
 ];
 
